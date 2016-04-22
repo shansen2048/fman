9904a64102dc67fcd1c12a42af7de8a6c604a5c8
Michael Herrmann <michael@herrmann.io>
2016-04-22 11:14:03 +0200

    Add desk file.

---
+++ bin/desk.sh
@@ -0,0 +1,36 @@
+# fman.sh
+#
+# Description: desk for doing work on fman.
+# (see https://github.com/jamesob/desk)
+
+# To install this file, symlink it to your local desks directory:
+#   ln -s <ABSOLUTE PATH TO THIS FILE> ~/.desk/desks/fman.sh
+# You can then switch to the desk with the command:
+#   desk . fman
+
+# Find the directory this script lies in, even when the script is called via a
+# symlink, as per the installation instructions above. Copied from
+# http://stackoverflow.com/a/246128/1839209:
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+THIS_SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+SRC_DIR="$THIS_SCRIPT_DIR/../.."
+
+cd "$SRC_DIR"
+source venv/bin/activate
+
+PS1="(fman) \h:\W \u\$ "
+
+# git add
+alias add='git add'
+
+# git commit
+alias commit='git commit'
+
+# git push
+alias push='git push'
