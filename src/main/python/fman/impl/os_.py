25fb1ef210408d4ac02ac9d9836248a567525cf9
Michael Herrmann <michael@herrmann.io>
2016-05-25 14:59:12 +0200

    Correct signature of abstract base class OS.

--- src/main/python/fman/impl/os_.py
+++ src/main/python/fman/impl/os_.py
@@ -3,14 +3,16 @@ from subprocess import Popen
 class OS:
 	def move_to_trash(self, *files):
 		raise NotImplementedError()
-	def prompt_user_to_pick_application(self):
+	def prompt_user_to_pick_application(self, message):
+		raise NotImplementedError()
+	def open(self, *files, with_app=None):
 		raise NotImplementedError()
 
 class OSX(OS):
 	def move_to_trash(self, *files):
 		from osxtrash import move_to_trash
 		return move_to_trash(*files)
-	def prompt_user_to_pick_application(self, title):
+	def prompt_user_to_pick_application(self, message):
 		# Note: This import takes 200ms. Don't do it at the top of a file!
 		from Cocoa import NSOpenPanel, NSURL, NSOKButton
 		panel = NSOpenPanel.openPanel()
