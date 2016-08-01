7a5d7c5aacf12256562af2c25513d7a21f9d8003
Michael Herrmann <michael@herrmann.io>
2016-08-01 16:18:56 +0200

    Bump version to 0.0.2

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ from os.path import join, isdir, isfile, islink
 from shutil import rmtree
 
 OPTIONS = {
-	'version': '0.0.1',
+	'version': '0.0.2',
 	'local_staticfiles_dir': '/Users/michael/dev/fman.io/static',
 	'server_staticfiles_dir': '/home/fman/src/static',
 	'server_user': 'fman@fman.io',
