db2fb5e6b5949c540eb3f87c36021b1d511b8e39
Michael Herrmann <michael@herrmann.io>
2016-08-02 12:33:20 +0200

    Add codesigning on Windows

--- build_impl/windows.py
+++ build_impl/windows.py
@@ -67,8 +67,21 @@ def exe():
 	for dll in ('msvcr100.dll', 'msvcr110.dll', 'msvcp110.dll'):
 		copy(join(r'c:\Windows\System32', dll), path('target/fman'))
 
+def sign_exe():
+	_sign(path('target/fman/fman.exe'), 'fman')
+
 def installer():
 	run(['makensis', path('src/main/Setup.nsi')])
+
+def sign_installer():
+	_sign(path('target/fman Setup.exe'), 'fman Setup')
+
+def _sign(file_path, description):
+	run([
+		'signtool', 'sign', '/f', path('conf/windows/michaelherrmann.pfx'),
+		'/p', 'Tu4suttmdpn!', '/tr', 'http://tsa.startssl.com/rfc3161',
+		'/d', description, '/du', 'https://fman.io', file_path
+	])
 
 def zip():
 	with ZipFile(path('target/fman.zip'), 'w', ZIP_DEFLATED) as zip:
