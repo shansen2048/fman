8718f4077f38a04149479dc52b8bb20023f2869b
Michael Herrmann <michael@herrmann.io>
2016-09-27 12:48:10 +0200

    Correct typo 'Moveing' in status bar to 'Moving'

--- src/main/resources/base/Plugins/Core/fileoperations.py
+++ src/main/resources/base/Plugins/Core/fileoperations.py
@@ -71,10 +71,11 @@ class FileTreeOperation:
 		)
 		return choice & YES or choice & YES_TO_ALL
 	def _report_processing_of_file(self, file_):
-		verbing = self.descr_verb.capitalize() + 'ing'
-		self.ui.show_status_message(
-			'%s %s...' % (verbing, basename(file_))
-		)
+		verb = self.descr_verb.capitalize()
+		if verb.endswith('e'):
+			verb = verb[:-1]
+		verbing = verb + 'ing'
+		self.ui.show_status_message('%s %s...' % (verbing, basename(file_)))
 	def perform_on_file(self, src, dest):
 		self._report_processing_of_file(src)
 		if exists(dest):
