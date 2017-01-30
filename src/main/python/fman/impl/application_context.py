54eeb7889f34ec497612e7ff205041eb499460c1
Michael Herrmann <michael@herrmann.io>
2017-01-30 15:32:20 +0100

    Only set runtime icon on Windows

--- src/main/python/fman/impl/application_context.py
+++ src/main/python/fman/impl/application_context.py
@@ -94,7 +94,7 @@ class ApplicationContext:
 		return self._app
 	@property
 	def app_icon(self):
-		if self._app_icon is None:
+		if self._app_icon is None and system.is_windows():
 			self._app_icon = QIcon(self.get_resource('fman.ico'))
 		return self._app_icon
 	@property
