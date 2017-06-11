4adfda720e10f6a4f7f9ca625f984fa4c5e066fa
Michael Herrmann <michael@herrmann.io>
2017-06-11 17:21:09 +0200

    Improve the tutorial for Linux

--- src/main/python/fman/impl/tutorial.py
+++ src/main/python/fman/impl/tutorial.py
@@ -70,8 +70,8 @@ class Tutorial:
 					"First things first: You'll be most productive when you "
 					"use fman with the keyboard. Try to resort to the mouse as "
 					"little as possible.",
-					"Ready? Let's try fman's most useful shortcut: "
-					"Press&nbsp;*%s*." % cmd_p
+					"Ready? Let's try fman's most useful shortcut:<br/>"
+					"Press *%s*." % cmd_p
 				],
 				{
 					'before': {
@@ -139,7 +139,7 @@ class Tutorial:
 				[
 					"Perfect! The files were selected. Here's a little "
 					"challenge for you: _De_select the files!",
-					"Hint: The shortcut for the Command Palette is %s."
+					"Hint: The shortcut for the Command Palette is <br/>*%s*."
 					% cmd_shift_p
 				],
 				{
