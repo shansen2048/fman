e82590b485d397ae3174f63d8a66f1e96560e5c8
Michael Herrmann <michael@herrmann.io>
2017-07-19 07:27:24 +0200

    Refactoring: Extract Settings class

--- src/main/python/fman/impl/session.py
+++ src/main/python/fman/impl/session.py
@@ -1,10 +1,9 @@
 from base64 import b64encode, b64decode
 from fman.util import system
-from json import JSONDecodeError
-from os import makedirs, getcwd
-from os.path import expanduser, dirname, realpath, normpath, splitdrive
+from fman.util.settings import Settings
+from os import getcwd
+from os.path import expanduser, realpath, normpath, splitdrive
 
-import json
 import sys
 
 class SessionManager:
@@ -13,31 +12,18 @@ class SessionManager:
 	DEFAULT_COLUMN_WIDTHS = [200, 75]
 
 	def __init__(self, json_path, fman_version, is_licensed):
-		self._json_path = json_path
+		self._settings = Settings(json_path)
 		self._fman_version = fman_version
 		self._is_licensed = is_licensed
-		self.is_first_run = False
-		try:
-			with open(self._json_path, 'r') as f:
-				self._json_dict = json.load(f)
-		except FileNotFoundError:
-			self._json_dict = {}
-			self.is_first_run = True
-		except JSONDecodeError:
-			self._json_dict = {
-				'fman_version': self._fman_version
-			}
-	@property
-	def previous_fman_version(self):
-		return self._json_dict.get('fman_version', None)
 	@property
 	def was_licensed_on_last_run(self):
-		return self._json_dict.get('is_licensed', None)
+		return self._settings.get('is_licensed', False)
 	def show_main_window(self, main_window):
 		self._restore_panes(main_window, sys.argv[1:])
 		self._restore_window_geometry(main_window)
 		main_window.show_status_message(self._get_startup_message())
-		if self.is_first_run:
+		is_first_run = not self._settings
+		if is_first_run:
 			main_window.showMaximized()
 		else:
 			# In this case, we assume that _restore_window_geometry restored the
@@ -45,7 +31,7 @@ class SessionManager:
 			# these, so call show() instead of showMaximized():
 			main_window.show()
 	def _restore_panes(self, main_window, paths_on_command_line):
-		panes = self._json_dict.get('panes', [{}] * self.DEFAULT_NUM_PANES)
+		panes = self._settings.get('panes', [{}] * self.DEFAULT_NUM_PANES)
 		for i, pane_info in enumerate(panes):
 			pane = main_window.add_pane()
 			try:
@@ -56,33 +42,31 @@ class SessionManager:
 			col_widths = pane_info.get('col_widths', self.DEFAULT_COLUMN_WIDTHS)
 			pane.set_column_widths(col_widths)
 	def _restore_window_geometry(self, main_window):
-		geometry_b64 = self._json_dict.get('window_geometry', None)
+		geometry_b64 = self._settings.get('window_geometry', None)
 		if geometry_b64:
 			main_window.restoreGeometry(_decode(geometry_b64))
-		window_state_b64 = self._json_dict.get('window_state', None)
+		window_state_b64 = self._settings.get('window_state', None)
 		if window_state_b64:
 			main_window.restoreState(_decode(window_state_b64))
 	def _get_startup_message(self):
-		previous_version = self.previous_fman_version
+		previous_version = self._settings.get('fman_version', None)
 		if not previous_version or previous_version == self._fman_version:
 			return 'v%s ready.' % self._fman_version
 		return 'Updated to v%s. ' \
 			   '<a href="https://fman.io/changelog?s=f">Changelog</a>' \
 			   % self._fman_version
 	def on_close(self, main_window):
-		self._json_dict['window_geometry'] = _encode(main_window.saveGeometry())
-		self._json_dict['window_state'] = _encode(main_window.saveState())
-		self._json_dict['panes'] = \
-			list(map(self._read_settings_from_pane, main_window.get_panes()))
-		self._json_dict['fman_version'] = self._fman_version
-		self._json_dict['is_licensed'] = self._is_licensed
+		self._settings['window_geometry'] = _encode(main_window.saveGeometry())
+		self._settings['window_state'] = _encode(main_window.saveState())
+		self._settings['panes'] = \
+			list(map(self._read_pane_settings, main_window.get_panes()))
+		self._settings['fman_version'] = self._fman_version
+		self._settings['is_licensed'] = self._is_licensed
 		try:
-			makedirs(dirname(self._json_path), exist_ok=True)
-			with open(self._json_path, 'w') as f:
-				json.dump(self._json_dict, f)
+			self._settings.flush()
 		except OSError:
 			pass
-	def _read_settings_from_pane(self, pane):
+	def _read_pane_settings(self, pane):
 		return {
 			'location': pane.get_path(),
 			'col_widths': pane.get_column_widths()
---
+++ src/main/python/fman/util/settings.py
@@ -0,0 +1,24 @@
+from json import JSONDecodeError
+from os import makedirs
+from os.path import dirname
+
+import json
+
+class Settings:
+	def __init__(self, json_path):
+		self._json_path = json_path
+		try:
+			with open(self._json_path, 'r') as f:
+				self._json_dict = json.load(f)
+		except (FileNotFoundError, JSONDecodeError):
+			self._json_dict = {}
+	def get(self, key, default):
+		return self._json_dict.get(key, default)
+	def __setitem__(self, key, value):
+		self._json_dict[key] = value
+	def flush(self):
+		makedirs(dirname(self._json_path), exist_ok=True)
+		with open(self._json_path, 'w') as f:
+			json.dump(self._json_dict, f)
+	def __bool__(self):
+		return bool(self._json_dict)
