3489ace7393b54abd31e6d60a020f53bd0dce10e
Michael Herrmann <michael@herrmann.io>
2017-10-17 18:22:46 +0200

    Add tests accidentally deleted in commit c9534e31

--- src/unittest/python/fman_unittest/impl/model/test___init__.py
+++ src/unittest/python/fman_unittest/impl/model/test___init__.py
@@ -1,5 +1,6 @@
-from fman.util import Signal
 from fman.impl.model import CachedFileSystem, FileSystemModel
+from fman.util import Signal
+from fman_unittest.impl.model import StubFileSystem
 from threading import Thread, Lock
 from time import sleep
 from unittest import TestCase
@@ -96,26 +97,3 @@ class FileSystemCountingIsdirCalls:
 		# Give other threads a chance to run:
 		sleep(.1)
 		return True
-
-class StubFileSystem:
-	def __init__(self, items):
-		self._items = items
-		self.file_changed = Signal()
-	def exists(self, item):
-		return item in self._items
-	def listdir(self, item):
-		return self._items[item].get('files', [])
-	def isdir(self, item):
-		return self._items[item].get('isdir', False)
-	def getsize(self, item):
-		return self._items[item].get('size', 1)
-	def getmtime(self, item):
-		return self._items[item].get('mtime', 1473339041.0)
-	def touch(self, item):
-		self._items[item] = {}
-	def mkdir(self, item):
-		self._items[item] = { 'isdir': True }
-	def rename(self, old_path, new_path):
-		self._items[new_path] = self._items.pop(old_path)
-	def delete(self, item):
-		del self._items[item]
