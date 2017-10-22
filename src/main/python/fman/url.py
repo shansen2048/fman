6a43baaa78751d30599fec271dd29680066e0bbb
Michael Herrmann <michael@herrmann.io>
2017-10-22 12:17:21 +0200

    Fix typo in comment

--- src/main/python/fman/url.py
+++ src/main/python/fman/url.py
@@ -16,6 +16,6 @@ def as_file_url(file_path):
 	# We purposely don't use Path#as_uri here because it escapes the URL.
 	# For instance: Path('/a b').as_uri() returns 'file:///a%20b'. The entire
 	# code base wuold get unnecessarily complicated if it had to escape URL
-	# charcates like %20 all the time. So we do not escape URLs and return
+	# characters like %20 all the time. So we do not escape URLs and return
 	# "file:///a b" instead:
 	return 'file://' + PurePath(file_path).as_posix()
