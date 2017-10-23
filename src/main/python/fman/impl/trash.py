e824f9447bfea28abe0b5ba11bae43a01237bd36
Michael Herrmann <michael@herrmann.io>
2017-10-23 18:10:48 +0200

    Fix restoring deleted files being broken on Ubuntu

--- src/main/python/fman/impl/trash.py
+++ src/main/python/fman/impl/trash.py
@@ -3,7 +3,11 @@ from fman import PLATFORM
 if PLATFORM == 'Mac':
 	from osxtrash import move_to_trash
 else:
-	from send2trash import send2trash
 	def move_to_trash(*files):
+		# We import send2trash as late as possible. Here's why: On Ubuntu
+		# (/Gnome), send2trash uses GIO - if it is available and initialized.
+		# Whether that happens is determined at *import time*. Importing
+		# send2trash at the last possible moment, ensures that it picks up GIO.
+		from send2trash import send2trash
 		for file in files:
 			send2trash(file)
