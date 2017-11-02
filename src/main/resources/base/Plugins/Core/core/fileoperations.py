92e6dd257dc9f1cf469b61ed1912575f9b16973b
Michael Herrmann <michael@herrmann.io>
2017-11-02 17:55:17 +0100

    Remove unnecessary code

--- src/main/resources/base/Plugins/Core/core/fileoperations.py
+++ src/main/resources/base/Plugins/Core/core/fileoperations.py
@@ -30,12 +30,6 @@ class FileTreeOperation:
 				break
 		self.ui.clear_status_message()
 	def _call_on_file(self, src):
-		if src.endswith('/'):
-			# File paths ending in '/' screw up os.path.basename(...):
-			raise ValueError(
-				"Please ensure file paths don't end in '/', eg. by calling "
-				"normpath(...)."
-			)
 		self._report_processing_of_file(src)
 		dest = self._get_dest_path(src)
 		try:
