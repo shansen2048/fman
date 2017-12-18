c70f280f2b694391519a92171a788748fc3e8f34
Michael Herrmann <michael@herrmann.io>
2017-12-18 10:44:10 +0100

    Simplify error message

--- fbs/conf.py
+++ fbs/conf.py
@@ -6,7 +6,5 @@ def path(relpath):
 	try:
 		project_dir = OPTIONS['project_dir']
 	except KeyError:
-		error_message = \
-			"Please set OPTIONS['project_dir'] before calling this function"
-		raise RuntimeError(error_message) from None
+		raise RuntimeError("Please set OPTIONS['project_dir']") from None
 	return normpath(join(project_dir, *relpath.split('/')))
