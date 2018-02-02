8d390df1903ca7f215651ea144cae45c72ad6754
Michael Herrmann <michael@herrmann.io>
2018-02-02 08:24:16 +0100

    Avoid multithreading error in Cache

--- src/main/python/fman/impl/fs_cache.py
+++ src/main/python/fman/impl/fs_cache.py
@@ -1,26 +1,34 @@
-from threading import RLock
+from threading import Lock, RLock
 from weakref import WeakValueDictionary
 
 class Cache:
 	def __init__(self):
 		self._items = {}
-		self._locks = WeakValueDictionary()
+		"""
+		We need a separate write lock to avoid spurious
+			"RuntimeError: dictionary changed size during iteration"
+		in clear(). The reason is that this method iterates over _items. If
+		another method (such as put(...)) updates _items at the same time, the
+		error occurs.
+		"""
+		self._write_lock = Lock()
+		self._read_locks = WeakValueDictionary()
 	def put(self, path, attr, value):
-		self._items.setdefault(path, {})[attr] = value
+		with self._write_lock:
+			self._items.setdefault(path, {})[attr] = value
 	def get(self, path, attr):
 		return self._items[path][attr]
 	def query(self, path, attr, compute_value):
-		with self._lock(path, attr):
+		with self._read_locks.setdefault((path, attr), RLock()):
 			try:
 				return self._items[path][attr]
 			except KeyError:
-				result = compute_value()
-				self._items.setdefault(path, {})[attr] = result
-				return result
+				value = compute_value()
+				self.put(path, attr, value)
+				return value
 	def clear(self, path):
-		self._items = {
-			other_path: value for other_path, value in self._items.items()
-			if other_path != path and not other_path.startswith(path + '/')
-		}
-	def _lock(self, path, attr):
-		return self._locks.setdefault((path, attr), RLock())
+		with self._write_lock:
+			self._items = {
+				other_path: value for other_path, value in self._items.items()
+				if other_path != path and not other_path.startswith(path + '/')
+			}
