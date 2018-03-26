f3658eb7799ac96627bae9c104f1b00afb7d7edd
Michael Herrmann <michael@herrmann.io>
2018-03-26 10:20:20 +0200

    Improve support of MoveToTrash for elementary OS

--- src/main/resources/base/Plugins/Core/core/trash.py
+++ src/main/resources/base/Plugins/Core/core/trash.py
@@ -4,10 +4,23 @@ if PLATFORM == 'Mac':
 	from osxtrash import move_to_trash
 else:
 	def move_to_trash(*files):
-		# We import send2trash as late as possible. Here's why: On Ubuntu
-		# (/Gnome), send2trash uses GIO - if it is available and initialized.
-		# Whether that happens is determined at *import time*. Importing
-		# send2trash at the last possible moment, ensures that it picks up GIO.
-		from send2trash import send2trash
+		send2trash = _import_send2trash()
 		for file in files:
 			send2trash(file)
+
+def _import_send2trash():
+	# We import send2trash as late as possible. Here's why: On Ubuntu
+	# (/Gnome), send2trash uses GIO - if it is available and initialized.
+	# Whether that happens is determined at *import time*. Importing
+	# send2trash at the last possible moment, ensures that it picks up GIO.
+	from send2trash import send2trash as result
+	if PLATFORM == 'Linux':
+		try:
+			from gi.repository import GObject
+		except ImportError:
+			pass
+		else:
+			# Fix for elementary OS / Pantheon:
+			if not hasattr(GObject, 'GError'):
+				from send2trash.plat_other import send2trash as result
+	return result
