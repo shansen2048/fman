e14870967ff131546b9cdbb7c4998efffe76c7b9
Michael Herrmann <michael@herrmann.io>
2018-03-30 09:57:46 +0200

    Show full tracebacks in excepthook despite PyQt

--- src/main/python/fman/impl/excepthook.py
+++ src/main/python/fman/impl/excepthook.py
@@ -7,6 +7,7 @@ from traceback import extract_tb
 import rollbar
 import sys
 import threading
+import traceback
 
 class Excepthook:
 	def __init__(self, plugin_dirs, plugin_error_handler):
@@ -25,14 +26,61 @@ class Excepthook:
 			)
 		else:
 			if not isinstance(exc_value, SystemExit):
-				self._handle_nonplugin_error(exc_type, exc_value, exc_tb)
+				enriched_tb = self._add_missing_frames(exc_tb)
+				self._handle_nonplugin_error(exc_type, exc_value, enriched_tb)
 	def _handle_nonplugin_error(self, exc_type, exc_value, exc_tb):
-		sys.__excepthook__(exc_type, exc_value, exc_tb)
+		# Normally, we would like to use sys.__excepthook__ here. But it doesn't
+		# work with our "fake" traceback (see _add_missing_frames(...)). The
+		# following call avoids this yet produces the same result:
+		traceback.print_exception(exc_type, exc_value, exc_tb)
 	def _get_plugin_causing_error(self, traceback):
 		for frame in extract_tb(traceback):
 			for plugin_dir in self._plugin_dirs:
 				if is_below_dir(frame.filename, plugin_dir):
 					return plugin_dir
+	def _add_missing_frames(self, tb):
+		"""
+		Let f and h be Python functions and g be a function of Qt. If
+			f() -> g() -> h()
+		(where "->" means "calls"), and an exception occurs in h(), then the
+		associated traceback does not contain f. This can hinder debugging.
+		To fix this, we create a "fake" traceback that contains the missing
+		entries.
+
+		The code below can be used to reproduce the f() -> g() -> h() problem.
+		It opens a window with a button. When you click it, an error occurs
+		whose traceback does not include f().
+
+			from PyQt5.QtWidgets import *
+			from PyQt5.QtCore import Qt
+
+			class Window(QWidget):
+				def __init__(self):
+					super().__init__()
+					btn = QPushButton('Click me', self)
+					btn.clicked.connect(self.f)
+				def f(self, _):
+					self.inputMethodQuery(Qt.ImAnchorPosition)
+				def inputMethodQuery(self, query):
+					if query == Qt.ImAnchorPosition:
+						# Make Qt call inputMethodQuery(ImCursorPosition).
+						# This is our "g()":
+						return super().inputMethodQuery(query) # "g()"
+					self.h()
+				def h(self):
+					raise Exception()
+
+			app = QApplication([])
+			window = Window()
+			window.show()
+			app.exec()
+		"""
+		result = fake_tb(tb.tb_frame, tb.tb_lasti, tb.tb_lineno, tb.tb_next)
+		frame = tb.tb_frame.f_back
+		while frame:
+			result = fake_tb(frame, frame.f_lasti, frame.f_lineno, result)
+			frame = frame.f_back
+		return result
 	def _enable_excepthook_for_threads(self):
 		"""
 		`sys.excepthook` isn't called for exceptions raised in non-main-threads.
@@ -88,5 +136,8 @@ class RollbarExcepthook(Excepthook):
 				'distribution': os_.distribution()
 			}
 		}
+
+fake_tb = \
+	namedtuple('fake_tb', ('tb_frame', 'tb_lasti', 'tb_lineno', 'tb_next'))
 
 RollbarRequest = namedtuple('RollbarRequest', ('user_id',))
