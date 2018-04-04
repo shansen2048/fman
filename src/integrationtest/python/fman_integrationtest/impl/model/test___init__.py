80d8b7e1c833add8a895105510234c0e0b143a26
Michael Herrmann <michael@herrmann.io>
2018-04-04 11:01:16 +0200

    Improve test case

--- src/integrationtest/python/fman_integrationtest/impl/model/test___init__.py
+++ src/integrationtest/python/fman_integrationtest/impl/model/test___init__.py
@@ -3,8 +3,9 @@ from core import Name, Size
 from fman.impl.model import SortedFileSystemModel
 from fman.impl.plugins.builtin import NullFileSystem, NullColumn
 from fman.impl.plugins.mother_fs import MotherFileSystem
-from fman.impl.util.qt import connect_once
+from fman.impl.util.qt import connect_once, DisplayRole, DecorationRole
 from fman.impl.util.qt.thread import run_in_main_thread
+from fman.url import splitscheme
 from fman_unittest.impl.model import StubFileSystem
 from threading import Event
 
@@ -19,6 +20,10 @@ class SortedFileSystemModelAT: # Instantiated in fman_integrationtest.test_qt
 		self.assertEqual('stub://', self._model.get_location())
 		loaded.wait()
 		self._expect_data([('dir', ''), ('file', '13 B')])
+		self._expect_data(
+			[(self._folder_icon, None), (self._file_icon, None)],
+			DecorationRole
+		)
 	def test_remove_current_dir(self):
 		self._set_location('stub://dir')
 		with self._wait_until_loaded():
@@ -33,16 +38,18 @@ class SortedFileSystemModelAT: # Instantiated in fman_integrationtest.test_qt
 		loaded = Event()
 		self._model.set_location(location, callback=loaded.set)
 		loaded.wait()
-	def _expect_data(self, expected):
+	def _expect_data(self, expected, role=DisplayRole):
 		self.assertEqual(len(expected), self._model.rowCount())
 		self.assertEqual(
 			len(expected[0]) if expected else 0, self._model.columnCount()
 		)
-		m = self._model
-		actual = [
-			tuple(m.data(m.index(row, col)) for col in range(m.columnCount()))
-			for row in range(m.rowCount())
-		]
+		model = self._model
+		actual = []
+		for row in range(self._model.rowCount()):
+			actual.append(tuple(
+				model.data(model.index(row, col), role)
+				for col in range(self._model.columnCount())
+			))
 		self.assertEqual(expected, actual)
 	@contextmanager
 	def _wait_until_loaded(self):
@@ -56,15 +63,24 @@ class SortedFileSystemModelAT: # Instantiated in fman_integrationtest.test_qt
 			self.fail('Timeout expired while waiting to location to be loaded.')
 	def setUp(self):
 		super().setUp()
-		self._fs = MotherFileSystem(StubIconProvider())
+		# N.B.: Normally we should have QIcon instances here. But they don't
+		# seem to work well with ==. So use strings instead:
+		folder_icon = '<folder icon>'
+		file_icon = '<file icon>'
+		files = {
+			'': {'is_dir': True, 'files': ['dir', 'file'], 'icon': folder_icon},
+			'dir': {'is_dir': True, 'files': ['subdir'], 'icon': folder_icon},
+			'dir/subdir': {'is_dir': True, 'icon': folder_icon},
+			'file': {'is_dir': False, 'size': 13, 'icon': file_icon}
+		}
+		self._folder_icon = folder_icon
+		self._file_icon = file_icon
+		self._fs = MotherFileSystem(StubIconProvider(files))
 		self._fs.add_child('null://', NullFileSystem())
 		self._register_column(NullColumn())
-		self._fs.add_child('stub://', StubFileSystem({
-			'': {'is_dir': True, 'files': ['dir', 'file']},
-			'dir': {'is_dir': True, 'files': ['subdir']},
-			'dir/subdir': {'is_dir': True},
-			'file': {'is_dir': False, 'size': 13}
-		}, default_columns=('core.Name', 'core.Size')))
+		stubfs = \
+			StubFileSystem(files, default_columns=('core.Name', 'core.Size'))
+		self._fs.add_child('stub://', stubfs)
 		self._register_column(Name(self._fs))
 		self._register_column(Size(self._fs))
 		self._model = self.run_in_app(
@@ -77,5 +93,8 @@ def _is_debugging():
 	return bool(sys.gettrace())
 
 class StubIconProvider:
+	def __init__(self, files):
+		self._files = files
 	def get_icon(self, url):
-		return None
+		path = splitscheme(url)[1]
+		return self._files[path].get('icon', None)
