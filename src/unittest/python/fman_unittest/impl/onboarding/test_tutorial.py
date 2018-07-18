a8ec12c8bde00e295ad49b965962de910b953ec7
Michael Herrmann <michael@herrmann.io>
2018-07-18 09:05:27 +0200

    Further refactoring

--- src/unittest/python/fman_unittest/impl/onboarding/test_tutorial.py
+++ src/unittest/python/fman_unittest/impl/onboarding/test_tutorial.py
@@ -6,17 +6,17 @@ from unittest import skipIf, TestCase
 
 class GetNavigationStepsTest(TestCase):
 	def test_self(self):
-		self.assertEqual([], _get_navigation_steps(self._root, self._root))
+		self._expect([], self._root, self._root)
 	def test_sub_dir(self):
-		self.assertEqual(
+		self._expect(
 			[('open', basename(dirname(self._root))),
 			 ('open', basename(self._root))],
-			_get_navigation_steps(self._root, dirname(dirname(self._root)))
+			self._root, dirname(dirname(self._root))
 		)
 	def test_go_up(self):
-		self.assertEqual(
+		self._expect(
 			[('go up', ''), ('open', 'util')],
-			_get_navigation_steps(join(dirname(self._root), 'util'), self._root)
+			join(dirname(self._root), 'util'), self._root
 		)
 	def test_wrong_scheme(self):
 		if is_windows():
@@ -24,53 +24,56 @@ class GetNavigationStepsTest(TestCase):
 		else:
 			root_drive = '/'
 
-		self.assertEqual(
+		self._expect(
 			[('go to', root_drive), ('open', 'test')],
-			_get_navigation_steps(join(as_url(root_drive), 'test'), 'null://')
+			join(as_url(root_drive), 'test'), 'null://'
 		)
 	@skipIf(not is_windows(), 'Skipping Windows-only test')
 	def test_switch_drives(self):
-		self.assertEqual(
+		self._expect(
 			[('show drives', ''), ('open', 'D:'), ('open', '64Bit')],
-			_get_navigation_steps(
-				as_url(r'D:\64Bit'), as_url(r'C:\Users\Michael')
-			)
+			as_url(r'D:\64Bit'), as_url(r'C:\Users\Michael')
 		)
 	@skipIf(not is_windows(), 'Skipping Windows-only test')
 	def test_start_from_drives(self):
-		self.assertEqual(
+		self._expect(
 			[('open', 'D:'), ('open', '64Bit')],
-			_get_navigation_steps(as_url(r'D:\64Bit'), 'drives://')
+			as_url(r'D:\64Bit'), 'drives://'
 		)
 	@skipIf(not is_windows(), 'Skipping Windows-only test')
 	def test_network_share(self):
 		from core.fs.local.windows.drives import DrivesFileSystem
-		self.assertEqual(
+		self._expect(
 			[('show drives', ''), ('open', DrivesFileSystem.NETWORK),
 			 ('open', 'SERVER'), ('open', 'Folder')],
-			_get_navigation_steps(
-				as_url(r'\\SERVER\Folder'), as_url(r'C:\Users\Michael')
-			)
+			as_url(r'\\SERVER\Folder'), as_url(r'C:\Users\Michael')
 		)
-		self.assertEqual(
+		self._expect(
 			[('open', 'SERVER'), ('open', 'Folder')],
-			_get_navigation_steps(as_url(r'\\SERVER\Folder'), 'network://')
+			as_url(r'\\SERVER\Folder'), 'network://'
 		)
 		# Say the user accidentally opened the wrong server:
-		self.assertEqual(
+		self._expect(
 			[('go up', ''), ('open', 'B'), ('open', 'Folder')],
-			_get_navigation_steps(as_url(r'\\B\Folder'), 'network://A')
+			as_url(r'\\B\Folder'), 'network://A'
 		)
-
 	def test_hidden_directory(self):
 		is_hidden = lambda url: True
 		dst_url = self._root
 		src_url = dirname(dst_url)
 		dir_name = basename(dst_url)
-		self.assertEqual(
+		self._expect(
 			[('toggle hidden files', dir_name), ('open', dir_name)],
-			_get_navigation_steps(dst_url, src_url, is_hidden, False)
+			dst_url, src_url, is_hidden, False
 		)
 	def setUp(self):
 		super().setUp()
 		self._root = dirname(as_url(__file__))
+	def _expect(
+		self, result, dst_url, src_url,
+		is_hidden=lambda url: False, showing_hidden_files=True
+	):
+		actual = _get_navigation_steps(
+			dst_url, src_url, is_hidden, showing_hidden_files
+		)
+		self.assertEqual(result, actual)
