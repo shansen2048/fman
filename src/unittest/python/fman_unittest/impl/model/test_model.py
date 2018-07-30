f1b30062044215d74d8f54dd089b4ca812c4faa2
Michael Herrmann <michael@herrmann.io>
2018-07-30 10:00:17 +0200

    Extract fman.impl.model.record_files

--- src/unittest/python/fman_unittest/impl/model/test_model.py
+++ src/unittest/python/fman_unittest/impl/model/test_model.py
@@ -1,17 +1,14 @@
 from fman.fs import Column
 from fman.impl.model import Model, Cell
-from fman.impl.model.model import File, _NOT_LOADED, \
-	get_moves_for_transforming, Lvl1SortValues
+from fman.impl.model.model import File, _NOT_LOADED
 from fman.impl.util.qt.thread import Executor
 from fman.url import splitscheme
 from fman_unittest.impl.model import StubFileSystem
-from itertools import permutations
 from PyQt5.QtCore import QObject, pyqtSignal
 from random import shuffle, random
 from unittest import TestCase
 
 import random
-import re
 
 class ModelRecordFilesTest(TestCase):
 	def test_load_file(self):
@@ -166,39 +163,3 @@ def c(str_, sort_value_asc=0, sort_value_desc=_NOT_LOADED):
 
 class StubApp(QObject):
 	aboutToQuit = pyqtSignal()
-
-class Lvl1SortValuesTest(TestCase):
-	def test_first_removed(self):
-		s = Lvl1SortValues(None, [0])
-		self.assertEqual(1, s._get_original_index(0))
-		self.assertEqual(2, s._get_original_index(1))
-
-class GetMovesForTransformingTest(TestCase):
-	def test_permutations(self):
-		max_str = 'ABCDEF'
-		for prefix_len in range(len(max_str) + 1):
-			str_ = max_str[:prefix_len]
-			for perm in permutations(str_):
-				self._test(str_, ''.join(perm))
-	def test_space(self):
-		self._test('A*BC', 'BA*C')
-	def test_move_space(self):
-		self._test('*AB', 'AB*')
-	def test_complex(self):
-		self._test(
-			'***F*****D***E***H*B*AC****G**',
-			'*A****B***C**DE*****F*G******H'
-		)
-	def _test(self, curr, goal):
-		moves = get_moves_for_transforming(self._parse(curr), self._parse(goal))
-		result = self._apply(moves, curr)
-		self.assertEqual(goal, result)
-	def _parse(self, str_):
-		return [(m.start(), m.group(0)) for m in re.finditer('[A-Z]', str_)]
-	def _apply(self, moves, to_str):
-		result = to_str
-		for src, dst in moves:
-			cut = result[src]
-			result = result[:src] + result[src+1:]
-			result = result[:dst] + cut + result[dst:]
-		return result
