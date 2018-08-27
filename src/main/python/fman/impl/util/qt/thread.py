b206d263e10b416ca93c19ff0156df2d09b295d8
Michael Herrmann <michael@herrmann.io>
2018-08-27 12:08:11 +0200

    Correct typo in comment

--- src/main/python/fman/impl/util/qt/thread.py
+++ src/main/python/fman/impl/util/qt/thread.py
@@ -77,7 +77,7 @@ class Executor:
 			# `sender.signal.emit()` gives spurious RuntimeErrors
 			# "wrapped C/C++ object of type Sender has been deleted".
 			# Split the line into its individual components - accessing .signal,
-			# accessing .emit and invoking emit(), to see in the stack trace
+			# accessing .emit and invoking emit() - to see in the stack trace
 			# which of them actually raises the error:
 			signal = sender.signal
 			emit = signal.emit
