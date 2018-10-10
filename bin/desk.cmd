093c00d709b03f435e8cc2b3c7558bd0d433186b
Michael Herrmann <michael@herrmann.io>
2018-10-10 10:02:58 +0200

    Fix/improve Windows desk script

--- bin/desk.cmd
+++ bin/desk.cmd
@@ -2,7 +2,8 @@ SET ROOT=c:\Users\Michael\dev\fman
 CD %ROOT%
 CALL venv\scripts\activate.bat
 DOSKEY clean=python build.py clean
-DOSKEY exe=python build.py exe $*
+DOSKEY freeze=python build.py freeze $*
 DOSKEY installer=python build.py installer $*
 DOSKEY test=python build.py test $*
+DOSKEY release=python build.py release $*
 DOSKEY run=python build.py run $*
