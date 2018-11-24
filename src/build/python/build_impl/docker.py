442fffa1b57c3a22c9333dc041d22cf3f6a62556
Michael Herrmann <michael@herrmann.io>
2018-11-24 12:42:35 +0100

    Refactor Docker build script

--- src/build/python/build_impl/docker.py
+++ src/build/python/build_impl/docker.py
@@ -14,38 +14,36 @@ def build_docker_image(name):
 	src_dir = path('src/build/docker/' + name)
 	filter_ = [path(f) for f in SETTINGS['files_to_filter']]
 	copy_with_filtering(src_dir, build_dir, files_to_filter=filter_)
-	for p in SETTINGS['docker_images'][name].get('build_files', []):
+	for p in _get_settings(name).get('build_files', []):
 		copy_with_filtering(path(p), build_dir, files_to_filter=filter_)
-	img_path = _get_docker_path(name)
 	subprocess.run(
-		['docker', 'build', '--pull', '-t', img_path, build_dir], check=True
+		['docker', 'build', '--pull', '-t', _get_docker_id(name), build_dir],
+		check=True
 	)
 
 def run_docker_image(name):
 	args = ['docker', 'run', '-it']
-	img_path = _get_docker_path(name)
-	for item in _get_docker_mounts(img_path).items():
-		args.append('-v')
-		args.append('%s:%s' % item)
-	args.append(img_path)
+	for item in _get_docker_mounts(name).items():
+		args.extend(['-v', '%s:%s' % item])
+	args.append(_get_docker_id(name))
 	subprocess.run(args)
 
-def _get_docker_path(image_name):
+def _get_docker_id(name):
 	prefix = SETTINGS['app_name'].replace(' ', '_').lower()
-	suffix = image_name.lower()
+	suffix = name.lower()
 	return prefix + '/' + suffix
 
-def _get_docker_mounts(img_path):
-	target_subdir = path('target/' + img_path.split('/')[1])
-	result = {
-		target_subdir: '/root/dev/fman/target'
-	}
+def _get_docker_mounts(name):
+	result = {'target/' + name.lower(): 'target'}
 	for file_name in listdir(path('.')):
-		file_path = path(file_name)
-		if _is_in_gitignore(file_path):
+		if _is_in_gitignore(path(file_name)):
 			continue
-		result[file_path] = '/root/dev/fman/' + file_name
-	return result
+		result[file_name] = file_name
+	path_in_docker = lambda p: '/root/%s/%s' % (name, p)
+	return {path(src): path_in_docker(dest) for src, dest in result.items()}
+
+def _get_settings(name):
+	return SETTINGS['docker_images'][name]
 
 def _is_in_gitignore(file_path):
 	process = subprocess.run(['git', 'check-ignore', file_path], stdout=DEVNULL)
