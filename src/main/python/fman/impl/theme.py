5b0d9666ab02e5c8bca6f45a40c06308d4b39149
Michael Herrmann <michael@herrmann.io>
2019-03-26 17:37:05 +0100

    Refactoring: Extract CSSEngine#parse_... methods

--- src/main/python/fman/impl/theme.py
+++ src/main/python/fman/impl/theme.py
@@ -1,6 +1,5 @@
 from collections import OrderedDict
 from fman.impl.util.css import parse_css, CSSEngine
-from PyQt5.QtGui import QColor
 from tinycss.parsing import ParseError
 
 class Theme:
@@ -22,7 +21,6 @@ class Theme:
 		self._css_rules = OrderedDict()
 		self._extra_qss_from_css = OrderedDict()
 		self._quicksearch_item_css = ''
-		self._css_engine = None
 		self._updates_enabled = False
 	def load(self, css_file_path):
 		with open(css_file_path, 'rb') as f:
@@ -61,40 +59,39 @@ class Theme:
 		self._updates_enabled = True
 		self._update_app()
 	def _get_quicksearch_item_css(self):
-		self._css_engine = \
-			CSSEngine([r for rs in self._css_rules.values() for r in rs])
+		engine = CSSEngine([r for rs in self._css_rules.values() for r in rs])
 		return {
 			'padding-top_px':
-				self._parse_px('.quicksearch-item', 'padding-top'),
+				engine.parse_px('.quicksearch-item', 'padding-top'),
 			'padding-left_px':
-				self._parse_px('.quicksearch-item', 'padding-left'),
+				engine.parse_px('.quicksearch-item', 'padding-left'),
 			'padding-right_px':
-				self._parse_px('.quicksearch-item', 'padding-right'),
+				engine.parse_px('.quicksearch-item', 'padding-right'),
 			'border-top-width_px':
-				self._parse_border_width('.quicksearch-item', 'border-top'),
+				engine.parse_border_width('.quicksearch-item', 'border-top'),
 			'border-bottom-width_px':
-				self._parse_border_width('.quicksearch-item', 'border-bottom'),
+				engine.parse_border_width('.quicksearch-item', 'border-bottom'),
 			'title': {
 				'font-size_pts':
-					self._parse_pts('.quicksearch-item-title', 'font-size'),
-				'color': self._parse_color('.quicksearch-item-title', 'color'),
+					engine.parse_pts('.quicksearch-item-title', 'font-size'),
+				'color': engine.parse_color('.quicksearch-item-title', 'color'),
 				'highlight': {
-					'color': self._parse_color(
+					'color': engine.parse_color(
 						'.quicksearch-item-title-highlight', 'color'
 					)
 				}
 			},
 			'hint': {
 				'font-size_pts':
-					self._parse_pts('.quicksearch-item-hint', 'font-size'),
-				'color': self._parse_color('.quicksearch-item-hint', 'color')
+					engine.parse_pts('.quicksearch-item-hint', 'font-size'),
+				'color': engine.parse_color('.quicksearch-item-hint', 'color')
 			},
 			'description': {
-				'font-size_pts': self._parse_pts(
+				'font-size_pts': engine.parse_pts(
 					'.quicksearch-item-description', 'font-size'
 				),
 				'color':
-					self._parse_color('.quicksearch-item-description', 'color')
+					engine.parse_color('.quicksearch-item-description', 'color')
 			}
 		}
 	def _css_rule_to_qss(self, rule):
@@ -119,49 +116,6 @@ class Theme:
 			return
 		qss = self._qss_base + ''.join(self._extra_qss_from_css.values())
 		self._app.set_style_sheet(qss)
-	def _parse_border_width(self, selector, declaration):
-		value = self._query(selector, declaration)
-		width = value.split(' ')[0]
-		error_message = \
-			'Invalid value for %s %s: %r. Should be of the form ' \
-			'"123px solid #ff0000".' % (selector, declaration, value)
-		if not width.endswith('px'):
-			raise ValueError(error_message)
-		try:
-			return int(width[:-2])
-		except ValueError:
-			raise ValueError(error_message) from None
-	def _parse_pts(self, selector, declaration):
-		value = self._query(selector, declaration)
-		error_message = \
-			'Invalid pt value for %s %s: %r' % (selector, declaration, value)
-		if not value.endswith('pt'):
-			raise ValueError(error_message)
-		try:
-			return int(value[:-2])
-		except ValueError:
-			raise ValueError(error_message) from None
-	def _parse_color(self, selector, declaration):
-		value = self._query(selector, declaration)
-		return QColor(value)
-	def _parse_px(self, selector, declaration):
-		value = self._query(selector, declaration)
-		error_message = \
-			'Invalid px value for %s %s: %r' % (selector, declaration, value)
-		if not value.endswith('px'):
-			raise ValueError(error_message)
-		try:
-			return int(value[:-2])
-		except ValueError:
-			raise ValueError(error_message) from None
-	def _query(self, selector, declaration):
-		declarations = self._css_engine.query(selector)
-		try:
-			return declarations[declaration]
-		except KeyError:
-			raise ValueError(
-				'Could not find %s for %s' % (declaration, selector)
-			)
 
 class ThemeError(Exception):
 	@property
--- src/main/python/fman/impl/util/css.py
+++ src/main/python/fman/impl/util/css.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from PyQt5.QtGui import QColor
 
 import tinycss
 
@@ -23,7 +24,50 @@ def parse_css(bytes_):
 class CSSEngine:
 	def __init__(self, parsed_css):
 		self._rules = parsed_css
-	def query(self, selector):
+	def parse_border_width(self, selector, declaration):
+		value = self._query(selector, declaration)
+		width = value.split(' ')[0]
+		error_message = \
+			'Invalid value for %s %s: %r. Should be of the form ' \
+			'"123px solid #ff0000".' % (selector, declaration, value)
+		if not width.endswith('px'):
+			raise ValueError(error_message)
+		try:
+			return int(width[:-2])
+		except ValueError:
+			raise ValueError(error_message) from None
+	def parse_pts(self, selector, declaration):
+		value = self._query(selector, declaration)
+		error_message = \
+			'Invalid pt value for %s %s: %r' % (selector, declaration, value)
+		if not value.endswith('pt'):
+			raise ValueError(error_message)
+		try:
+			return int(value[:-2])
+		except ValueError:
+			raise ValueError(error_message) from None
+	def parse_color(self, selector, declaration):
+		value = self._query(selector, declaration)
+		return QColor(value)
+	def parse_px(self, selector, declaration):
+		value = self._query(selector, declaration)
+		error_message = \
+			'Invalid px value for %s %s: %r' % (selector, declaration, value)
+		if not value.endswith('px'):
+			raise ValueError(error_message)
+		try:
+			return int(value[:-2])
+		except ValueError:
+			raise ValueError(error_message) from None
+	def _query(self, selector, declaration):
+		declarations = self._get_declarations(selector)
+		try:
+			return declarations[declaration]
+		except KeyError:
+			raise ValueError(
+				'Could not find %s for %s' % (declaration, selector)
+			)
+	def _get_declarations(self, selector):
 		result = {}
 		for rule in self._rules:
 			for sel in rule.selectors:
