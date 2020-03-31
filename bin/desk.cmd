f4e0cdee41f12d8119ae70318db036d5dfc30c38
Michael Herrmann <michael@herrmann.io>
2020-03-31 13:54:39 +0200

    Improve desk script

--- bin/desk.cmd
+++ bin/desk.cmd
@@ -6,5 +6,5 @@ DOSKEY clean=python build.py clean
 DOSKEY freeze=python build.py freeze $*
 DOSKEY installer=python build.py installer $*
 DOSKEY sign_installer=python build.py sign_installer $*
-DOSKEY test=python build.py test $*
+DOSKEY tests=python build.py test $*
 DOSKEY release=python build.py release $*
